cb2230e0444d0735840491c5d1ce8d73113e59ad
ryancloude <ryancloude@gmail.com>
2025-09-22 15:25:29 -0400

    updated DBT staging tables and sources

---
+++ dbt/models/staging/stg_injury_spans.sql
@@ -0,0 +1,27 @@
+{{config(materialized='view',
+        schema='silver')}}
+
+with src as (
+    select * from {{source('silver','injury_spans')}}
+)
+
+select
+    {{dbt_utils.generate_surrogate_key(['person_id','il_place_date']) }} as injury_sk,
+    il_trans_id,
+    il_place_date,
+    person_id,
+    il_place_team,
+    raw_injury,
+    side,
+    body_part,
+    injury_type,
+    body_part_group,
+    seconde_raw_injury,
+    second_body_part,
+    second_injury_type,
+    return_trans_id,
+    return_date,
+    return_team,
+    injury_length,
+    currently_injured
+from src
---
+++ dbt/models/staging/stg_roster_entries.sql
@@ -0,0 +1,19 @@
+{{config(materialized='view',
+        schema='bronze')}}
+
+with src as (
+    select * from {{source('bronze','roster_entries')}}
+)
+
+select
+    {{dbt_utils.generate_surrogate_key(['person_id','team_id','start_date','status_code']) }} as roster_sk,
+    person_id,
+    team_id,
+    start_date,
+    status_date,
+    end_date,
+    is_active,
+    status_code,
+    status_desc,
+    parent_org_id
+from src
